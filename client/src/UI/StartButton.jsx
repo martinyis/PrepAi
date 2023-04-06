7603a6bbdcc124ee803c097f61bb9f1d7738537b
martinyis <martinyis11@gmail.com>
2023-04-05 20:50:40 -0400

    Change styles fro start-button

---
+++ client/src/UI/StartButton.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button } from "@mui/material";
+const StartButton = (props) => {
+  const { text } = props;
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      sx={{
+        borderRadius: "10px",
+        width: "100%",
+        height: "50px",
+        backgroundColor: "#235c72",
+      }}
+      style={{
+        backgroundColor: "#235c72",
+      }}
+    >
+      {text}
+    </Button>
+  );
+};
+
+export default StartButton;
