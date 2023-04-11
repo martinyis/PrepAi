4b2bade7b02e4367b8027a1096dc95db813417a7
subhanabdi <subhanabidi211@gmail.com>
2023-04-11 05:23:58 +0500

    Button issue main solve this Button issue

--- client/src/UI/CreateButton.jsx
+++ client/src/UI/CreateButton.jsx
@@ -3,26 +3,27 @@ import { Button } from "@mui/material";
 import { Link, Typography } from "@material-ui/core";
 const CreateButton = () => {
   return (
-    <Link to="create">
-      <Button
-        variant="contained"
-        color="primary"
-        sx={{
-          width: "60%",
-          height: "40px",
-          backgroundColor: "#235c72",
-          maxWidth: "200px",
-          marginBottom: "100px",
-        }}
-        style={{
-          backgroundColor: "#235c72",
-          color: "#9dcad4",
-          fontSize: "15px",
-        }}
-      >
-        Start
-      </Button>
-    </Link>
+    <Link to="/create-prep">
+    <Button
+      variant="contained"
+      color="primary"
+      sx={{
+        width: "60%",
+        height: "40px",
+        backgroundColor: "#235c72",
+        maxWidth: "200px",
+        marginBottom: "100px",
+      }}
+      style={{
+        backgroundColor: "#235c72",
+        color: "#9dcad4",
+        fontSize: "15px",
+      }}
+    >
+      Start
+    </Button>
+  </Link>
+  
   );
 };
 
