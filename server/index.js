af1b3c737a129bd8d641f4b71b66b67cb07451cf
martinyis <martinyis11@gmail.com>
2023-04-10 22:33:23 -0400

    Comment out try catch index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
 const configuration = new Configuration({
   organization: "org-rbaMvLZLKC7vPLKOY0myB4SQ",
-  apiKey: process.env.OPENAI_API_KEY,
+  // apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 const bodyParser = require("body-parser");
@@ -20,19 +20,19 @@ const port = 5000;
 app.post("/", async (req, res) => {
   const { inputValue } = req.body;
   console.log(inputValue);
-  try {
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${inputValue}`,
-      max_tokens: 500,
-      temperature: 0,
-    });
-    res.json({
-      answer: response.data.choices[0].text,
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  // try {
+  //   const response = await openai.createCompletion({
+  //     model: "text-davinci-003",
+  //     prompt: `${inputValue}`,
+  //     max_tokens: 500,
+  //     temperature: 0,
+  //   });
+  //   res.json({
+  //     answer: response.data.choices[0].text,
+  //   });
+  // } catch (error) {
+  //   console.log(error);
+  // }
 });
 
 app.listen(port, () => {
