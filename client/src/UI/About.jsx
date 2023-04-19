4a2152a32f8673da3d65c41c5094facbbf4d2d27
Deepak Singh <deepkushwah824@gmail.com>
2023-04-20 00:42:42 +0530

    #29 improved the design.
    Here are some key changes:
    
    The background color has been changed to a lighter shade to make the text stand out more.
    The font size has been increased and the line spacing has been reduced for improved readability.
    The text is now left-aligned for easier reading.
    A large, high-quality image has been added to make the page more visually appealing.
    A new color palette has been used to make the page look more modern and cohesive.
    Overall, these changes should make the About section more engaging and appealing to users while maintaining readability and usability.

--- client/src/UI/About.jsx
+++ client/src/UI/About.jsx
@@ -1,39 +1,72 @@
 import { Typography } from "@material-ui/core";
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  aboutContainer: {
+    maxWidth: "1000px",
+    width: "100%",
+    margin: "0 auto",
+    marginTop: 100,
+    backgroundColor: "#F2F2F2",
+    borderRadius: "10px",
+    padding: "20px",
+  },
+  aboutTitle: {
+    fontWeight: "bold",
+    fontSize: "3rem",
+    marginBottom: "2rem",
+    color: theme.palette.primary.main,
+    textAlign: "center",
+  },
+  aboutText: {
+    letterSpacing: "0.05rem",
+    lineHeight: "1.5",
+    fontSize: "1.2rem",
+    textAlign: "justify",
+    color: "#444",
+    marginTop: "2rem",
+  },
+  aboutImageContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "2rem",
+  },
+  aboutImage: {
+    height: "auto",
+    width: "80%",
+    maxWidth: "800px",
+    borderRadius: "10px",
+    boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.15)",
+  },
+}));
 
 const About = () => {
+  const classes = useStyles();
+
   return (
-    <div
-      style={{
-        maxWidth: "1000px",
-        width: "100%",
-        margin: "0 auto",
-        marginTop: 100,
-        backgroundColor: "rgb(14, 43, 61, 1)",
-        borderRadius: "10px",
-        padding: "20px",
-      }}
-    >
-      <Typography variant="h1">About</Typography>
-      <Typography
-        variant="caption"
-        style={{
-          //letter spacing
-          letterSpacing: "0.05rem",
-          //line spacing
-          lineHeight: "2.5em",
-          //center text
-          textAlign: "right",
-        }}
-      >
-        Welcome to our website, a powerful tool designed to help students
-        prepare for their exams, quizzes, and academic assessments. Our website
-        utilizes the cutting-edge OpenAI API to organize the data you provide
-        and make it easy to prepare, allowing you to optimize your study time
-        and perform better on your exams. Our website offers a variety of
-        features to help you prepare, including customizable quizzes, practice
-        exams, study guides, and more.
+    <div className={classes.aboutContainer}>
+      <Typography variant="h1" className={classes.aboutTitle}>
+        About
       </Typography>
+      <Typography variant="body1" className={classes.aboutText}>
+        At our website, we believe that studying should be an engaging and
+        rewarding experience. That's why we've created a platform that leverages
+        the power of cutting-edge technologies to help students prepare for
+        their exams and quizzes with ease. Our platform uses the OpenAI API to
+        analyze the data you provide and create customized quizzes, practice
+        exams, and study guides that cater to your unique learning style. We
+        believe that with the right tools and resources, anyone can achieve
+        their academic goals.
+      </Typography>
+      <div className={classes.aboutImageContainer}>
+        <img
+          // src="https://picsum.photos/800/600"
+          alt="Students studying"
+          className={classes.aboutImage}
+        />
+      </div>
     </div>
   );
 };
