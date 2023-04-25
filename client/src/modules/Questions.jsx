d80616a2648040b5e98eccc1b9174010e6aa7b49
martinyis <martinyis11@gmail.com>
2023-04-25 08:40:13 -0400

    Get rid of useless comments

--- client/src/modules/Questions.jsx
+++ client/src/modules/Questions.jsx
@@ -9,21 +9,6 @@ const Questions = (props) => {
   const { questions } = props;
   return (
     <div>
-      {/* <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Accordion 1</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion> */}
       {questions.map((question, id) => (
         <Accordion>
           <AccordionSummary
